f3d61f27ca46046b1bb22f52547d38896272d4da
Akaid Khan <43861949+akaidkhan@users.noreply.github.com>
2019-08-31 18:17:59 +0600

    Initial commit

---
+++ app/src/main/java/com/akaidkhan/floating_action_button/MainActivity.java
@@ -0,0 +1,27 @@
+package com.akaidkhan.floating_action_button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton mFab = findViewById(R.id.fab);
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "FAB is clicked...", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
